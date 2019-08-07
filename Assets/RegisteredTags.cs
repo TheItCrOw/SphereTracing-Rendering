d6f061ce8d43a20a4e898ca93db0b2d17b95a1ce
TheItCrOw <keboen@web.de>
2019-08-07 18:57:43 +0200

    Initial Commit

---
+++ Assets/RegisteredTags.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RegisteredTags
+{
+
+    public static List<string> WellKnownInteractableTags = new List<string>()
+    {
+        "GameObject"
+
+    };
+
+
+
+}
